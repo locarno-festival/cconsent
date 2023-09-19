c990620e2b1fdb236658014f47509d67a5e6332c
Alessio.Vairetti <alessio.vairetti@locarnofestival.ch>
2023-09-19 12:42:24 +0200

    Palmares update

--- src/cookieconsent.config.src.js
+++ src/cookieconsent.config.src.js
@@ -209,7 +209,6 @@ loadGTM = function(){
 								j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 								'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 								})(window,document,'script','dataLayer','GTM-N47WHX2');
-								
 	gtmloaded=true;
 	}
 }
